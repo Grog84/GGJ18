49eafc29857b086e7f38b88f9cc4081d138ef76e
DarkTiger <darktigerdevelop@gmail.com>
2018-01-27 19:11:13 +0100

    Improved some props

--- Assets/Scripts/ScientistScript.cs
+++ Assets/Scripts/ScientistScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ScientistScript : MonoBehaviour
 {
-    public float speed = 1;
+    //public float speed = 1;
+    public float secondsToWait = 10;
+    public float durationOfWait = 4;
+    public float speed = 25;
     bool leftMovement = true;
     Rigidbody2D rb;
     Vector3 pointA;
     Vector3 pointB;
+    float currentSpeed = 0;
 
 
     void Start()
@@ -18,10 +22,13 @@ public class ScientistScript : MonoBehaviour
         transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
         pointA = transform.GetChild(0).transform.position;
         pointB = transform.GetChild(1).transform.position;
+        currentSpeed = speed;
     }
 
-    void Update()
+    void LateUpdate()
     {
+        StartCoroutine(Pause());
+
         if (transform.position.x <= pointA.x || transform.position.x >= pointB.x)
         {
             leftMovement = !leftMovement;
@@ -31,14 +38,22 @@ public class ScientistScript : MonoBehaviour
 
         if (leftMovement)
         {
-            pos.x -= (speed/100) * Time.deltaTime;
+            pos.x -= (currentSpeed/100) * Time.deltaTime;
         }
         else
         {
-            pos.x += (speed/100) * Time.deltaTime;
+            pos.x += (currentSpeed/100) * Time.deltaTime;
         }
 
         transform.position = pos;
+    }
+
+    IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        currentSpeed = 0;
+        yield return new WaitForSeconds(durationOfWait);
+        currentSpeed = speed;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
