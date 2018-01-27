63110e1fc00fb9e2c59fccae5dc67b841ce91940
Unknown <robertolarde@gmail.com>
2018-01-27 23:43:48 +0100

    Added new check ground strategy

---
+++ Assets/Scripts/DocCollisionDetection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocCollisionDetection : MonoBehaviour {
+
+    public AlienRay alienRay;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.layer.ToString());
+        if (other.gameObject.layer == 8)
+        {
+            alienRay.hasReachedGround = true;
+        }
+       
+    }
+
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.layer.ToString());
+        if (other.gameObject.layer == 8)
+        {
+            alienRay.hasReachedGround = false;
+        }
+        else
+        {
+
+        }
+    }
+}
