a4ec1bcac3182249f2068ac6897f580bb351ce97
Fabio Brigidi <fabio.brigidi@gmal.com>
2018-01-27 09:41:10 +0100

    Uploaded AlienRay script

---
+++ Assets/Scripts/AlienRay.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienRay : MonoBehaviour
+{
+    public HingeJoint2D heelR;
+    public HingeJoint2D heelL;
+    Rigidbody2D rbHellR;
+    Rigidbody2D rbHellL;
+    public Rigidbody2D body;
+    public Transform alien;
+
+
+    void Start()
+    {
+        rbHellR = heelR.GetComponent<Rigidbody2D>();
+        rbHellL = heelL.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(Vector3.right * 100);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(Vector3.left * 100);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(Vector3.up * 100);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = alien.position;
+        pos.x = body.position.x;
+        alien.position = pos;
+    }
+}
