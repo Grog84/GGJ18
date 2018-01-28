9ade7031f0381070159aa020ca73ce0eb287c0ab
DarkTiger <darktigerdevelop@gmail.com>
2018-01-28 01:46:06 +0100

    Added new switch type

---
+++ Assets/Scripts/SwitchStrangeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchStrangeScript : MonoBehaviour
+{
+    public GameObject objectToActive = null;
+    bool isActive = false;
+    GameObject switchStrange;
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
+        switchStrange = transform.GetChild(0).gameObject;
+    }
+	
+	
+	void Update ()
+    {
+        if (!isActive)
+        {
+            if (switchStrange.transform.localPosition.x <= -1)
+            {
+                isActive = true;
+                rb.bodyType = RigidbodyType2D.Static;
+
+                if (objectToActive != null)
+                {
+                    objectToActive.GetComponent<DoorScript>().isActive = true;
+                }
+            }
+        }
+    }
+}
