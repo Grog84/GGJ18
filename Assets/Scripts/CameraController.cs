833daaf65eaf72f498e6e36d4ed206dde9ca15f2
Fabio Brigidi <fabio.brigidi@gmal.com>
2018-01-28 10:18:37 +0100

    Created prefab copy of the scientist

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,50 +6,63 @@ using Chronos;
 
 public class CameraController : MonoBehaviour {
 
-    public Transform[] fixedPositions;
-    int currentAreaIdx = 0;
-    //public LocalClock[] localClocks;
-    Vector3 nextPosition;
-
-    bool isLerping = false;
+    public Transform player;
 
     private void Start()
     {
-        nextPosition = new Vector3(fixedPositions[currentAreaIdx].position.x, fixedPositions[currentAreaIdx].position.y, -10f);
-        transform.DOMove(nextPosition, 2f);
-        //localClocks = FindObjectsOfType<LocalClock>();
+        player = FindObjectOfType<GrabSystem>().transform;
     }
 
-    public void MoveToArea(int idx)
+    private void LateUpdate()
     {
-        if (idx != currentAreaIdx)
-        {
-            isLerping = true;
-            currentAreaIdx = idx;
-            nextPosition = new Vector3(fixedPositions[currentAreaIdx].position.x, fixedPositions[currentAreaIdx].position.y, -10f);
-            transform.DOMove(nextPosition, 2f);
-        }
+        transform.position = new Vector3(player.position.x , player.position.y, -10f);
     }
 
-    //private void Update()
+    //OLD
+    //public Transform[] fixedPositions;
+    //int currentAreaIdx = 0;
+    ////public LocalClock[] localClocks;
+    //Vector3 nextPosition;
+
+    //bool isLerping = false;
+
+    //private void Start()
     //{
-    //    if (isLerping)
-    //    {
-    //        foreach (var clock in localClocks)
-    //        {
-    //            clock.paused = true;
-    //        }
-
-    //        if (Vector3.Distance(transform.position, nextPosition) < 0.1)
-    //        {
-    //            isLerping = false;
-    //            foreach (var clock in localClocks)
-    //            {
-    //                clock.paused = false;
-    //            }
-    //        }
-    //    }
-
+    //    nextPosition = new Vector3(fixedPositions[currentAreaIdx].position.x, fixedPositions[currentAreaIdx].position.y, -10f);
+    //    transform.DOMove(nextPosition, 2f);
+    //    //localClocks = FindObjectsOfType<LocalClock>();
     //}
+
+    //public void MoveToArea(int idx)
+    //{
+    //    if (idx != currentAreaIdx)
+    //    {
+    //        isLerping = true;
+    //        currentAreaIdx = idx;
+    //        nextPosition = new Vector3(fixedPositions[currentAreaIdx].position.x, fixedPositions[currentAreaIdx].position.y, -10f);
+    //        transform.DOMove(nextPosition, 2f);
+    //    }
+    //}
+
+    ////private void Update()
+    ////{
+    ////    if (isLerping)
+    ////    {
+    ////        foreach (var clock in localClocks)
+    ////        {
+    ////            clock.paused = true;
+    ////        }
+
+    ////        if (Vector3.Distance(transform.position, nextPosition) < 0.1)
+    ////        {
+    ////            isLerping = false;
+    ////            foreach (var clock in localClocks)
+    ////            {
+    ////                clock.paused = false;
+    ////            }
+    ////        }
+    ////    }
+
+    ////}
 
 }
