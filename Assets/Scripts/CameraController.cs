f71928b85e809c33c61c6c9d7670b3f86dfbff5e
Fabio Brigidi <fabio.brigidi@gmal.com>
2018-01-28 03:38:06 +0100

    Implemented chronos and camera scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Chronos;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform[] fixedPositions;
+    int currentAreaIdx = 0;
+    public LocalClock[] localClocks;
+
+    bool isLerping = false;
+
+    private void Start()
+    {
+        transform.DOMove(fixedPositions[currentAreaIdx].position, 2f);
+        localClocks = FindObjectsOfType<LocalClock>();
+    }
+
+    public void MoveToArea(int idx)
+    {
+        if (idx != currentAreaIdx)
+        {
+            isLerping = true;
+            currentAreaIdx = idx;
+            transform.DOMove(fixedPositions[currentAreaIdx].position, 2f);
+        }
+    }
+
+    private void Update()
+    {
+        if (isLerping)
+        {
+            foreach (var clock in localClocks)
+            {
+                clock.paused = true;
+            }
+
+            if (Vector3.Distance(transform.position, fixedPositions[currentAreaIdx].position) < 0.1)
+            {
+                isLerping = false;
+                foreach (var clock in localClocks)
+                {
+                    clock.paused = false;
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+
+    public int idx;
+    CameraController cameraController;
+
+    private void Start()
+    {
+        cameraController = FindObjectOfType<CameraController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            cameraController.MoveToArea(idx);
+        }
+    }
+}
